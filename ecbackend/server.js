2ad74fb8572e657d77c0ad4983ec611cd891554c
JuanCa <juank060790@gmail.com>
2021-01-03 12:31:23 +0700

    secobd commmit

---
+++ ecbackend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import productRouter from "./routers/productRouter.js";
+import userRouter from "./routers/userRouter.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazon", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.get("/", (req, res) => {
+  res.send("server is ready");
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server at htttp://localhost:${port}`);
+});
---
+++ ecfrontend/src/Screens/HomeScreen.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import Product from "../Components/Product";
+import LoadingBox from "../Components/LoadingBox";
+import MessageBox from "../Components/MessageBox";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../redux/actions/ProductsActions";
+
+export default function HomeScreen() {
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+  return (
+    <div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Product key={product._id} product={product}></Product>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
